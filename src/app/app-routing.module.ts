411b732559c53cf45d0c9d3941ede6f1f0da4e3c
tonovarela <tonovarela@live.com>
2018-10-17 00:07:11 -0500

    Proyecto base

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { dashboardRoutes } from './dashboard/dashboard.routes';
+import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+ { path: 'login',  component: LoginComponent},
+ { path: 'register', component: RegisterComponent},
+ { path: '', component: DashboardComponent, children: dashboardRoutes},
+ { path: '**', redirectTo: ''}
+];
+
+@NgModule( {
+    imports: [
+        RouterModule.forRoot (routes)
+    ],
+    exports: [
+        RouterModule
+
+    ]
+})
+
+
+export class AppRoutingModule {}
---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,11 @@
+import { EstadisticaComponent } from './../ingreso-egreso/estadistica/estadistica.component';
+import { Routes } from '@angular/router';
+import { IngresoEgresoComponent } from '../ingreso-egreso/ingreso-egreso.component';
+import { DetalleComponent } from '../ingreso-egreso/detalle/detalle.component';
+
+
+export const dashboardRoutes: Routes = [
+ { path: '', component: EstadisticaComponent},
+ { path: 'ingreso-egreso', component: IngresoEgresoComponent},
+ { path: 'detalle', component: DetalleComponent}
+];
